7d1e011ec23bb671214a45f12a60aafa70ad157d
Cian Butler <butlerx@notthe.cloud>
2026-01-17 10:51:52 +0000

    Bump deps
    empty slide deck

---
+++ eleventy.config.images.mjs
@@ -0,0 +1,35 @@
+import path from 'node:path';
+import eleventyImage from '@11ty/eleventy-img';
+
+export default function images(config) {
+  // Eleventy Image shortcode
+  // https://www.11ty.dev/docs/plugins/image/
+  config.addAsyncShortcode(
+    'image',
+    async function imageShortcode(src, alt, widths, sizes) {
+      // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+      // Warning: Avif can be resource-intensive so take care!
+      let formats = ['avif', 'webp', 'auto'];
+      let file = relativeToInputPath(this.page.inputPath, src);
+      let metadata = await eleventyImage(file, {
+        widths: widths || ['auto'],
+        formats,
+        outputDir: path.join(config.dir.output, 'img'), // Advanced usage note: `config.dir` works here because we’re using addPlugin.
+      });
+      // TODO loading=eager and fetchpriority=high
+      let imageAttributes = {
+        alt,
+        sizes,
+        loading: 'lazy',
+        decoding: 'async',
+      };
+      return eleventyImage.generateHTML(metadata, imageAttributes);
+    },
+  );
+}
+
+function relativeToInputPath(inputPath, relativeFilePath) {
+  let split = inputPath.split('/');
+  split.pop();
+  return path.resolve(split.join(path.sep), relativeFilePath);
+}
--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,13 +1,12 @@
-const markdownIt = require('markdown-it');
-const { EleventyHtmlBasePlugin } = require('@11ty/eleventy');
-const pluginBundle = require('@11ty/eleventy-plugin-bundle');
-const pluginReveal = require('./eleventy.config.reveal.js');
-const pluginImages = require('./eleventy.config.images.js');
+import markdownIt from 'markdown-it';
+import { EleventyHtmlBasePlugin } from '@11ty/eleventy';
+import pluginBundle from '@11ty/eleventy-plugin-bundle';
+import pluginReveal from './eleventy.config.reveal.mjs';
+import pluginImages from './eleventy.config.images.mjs';
 
 const md = new markdownIt({ html: true });
-const imageTypes = '';
 
-module.exports = (config) => {
+export default function (config) {
   config.addWatchTarget('content/**/*.{svg,webp,png,jpeg,jpg}');
   config.addPassthroughCopy('content/img');
   config.addPlugin(pluginReveal);
@@ -15,9 +14,7 @@ module.exports = (config) => {
   config.addPlugin(EleventyHtmlBasePlugin);
   config.addPlugin(pluginBundle);
   config.setServerPassthroughCopyBehavior('passthrough');
-
   config.addFilter('markdown', (content) => md.render(content));
-
   return {
     templateFormats: ['md', 'njk', 'html'],
     dataTemplateEngine: 'njk',
@@ -28,4 +25,4 @@ module.exports = (config) => {
       output: '_site',
     },
   };
-};
+}
